9b53f2675597487a9365c183342949a69c350b62
Yannick Melzer <yannick@fam-melzer.de>
2024-04-29 21:38:00 +0200

    Generate js file from protobuffer

---
+++ frontend/src/routes/storage/new/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { storage as StorageFlatbuffer } from '$lib/models/storage';
+
+  let storage = StorageFlatbuffer.Storage.create({ name: 'my test storage' });
+</script>
+
+<h1>New Storage</h1>
