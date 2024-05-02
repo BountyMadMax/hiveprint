c160e2f429553b191dc7c03ca1b7777a5496994a
Yannick Melzer <yannick@fam-melzer.de>
2024-05-02 19:16:08 +0200

    Add frontend-vue as a possible replacement for svelte

---
+++ frontend-vue/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { ref, reactive } from 'vue';
+
+  const count = ref(0);
+
+  const state = reactive({ count: 0 });
+
+  function increment() {
+    count.value++;
+  }
+</script>
+
+<template>
+  <button @click="increment">
+    Click {{ count }}
+  </button>
+
+  <button @click="state.count++">
+    Reactive {{ state.count }}
+  </button>
+</template>
