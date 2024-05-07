c1e0a0ab2885e8744a7f2e3093657380464fa8b8
Yannick Melzer <yannick@fam-melzer.de>
2024-05-07 13:40:13 +0200

    Replace sveltekit frontend with nuxt frontend

--- frontend/src/routes/storage/new/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { storage as StorageFlatbuffer } from '$lib/models/storage';
-
-  let storage = StorageFlatbuffer.Storage.create({ name: 'my test storage' });
-</script>
-
-<div class='m-10'>
-  <h1 class='font-bold text-3xl my-4'>New Storage</h1>
-</div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
   theme: {
     extend: {},
+    fontFamily: {
+      serif: ['Inter Variable', 'serif'],
+    },
   },
   plugins: [],
 }
-
