962cd5a78f07c9372e92269538f8ecf0f7589c40
Yannick Melzer <yannick@fam-melzer.de>
2024-11-21 18:58:58 +0100

    Render template on HTMX request, else wrap it in layout

--- main.go
+++ main.go
@@ -3,12 +3,26 @@ package main
 import (
 	"context"
 	"hiveprint/src/views"
+	"hiveprint/src/views/pages"
+	"hiveprint/src/views/pages/models"
 	"net/http"
 
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+func addStaticFiles(e *echo.Echo) {
+	// HTMX
+	e.File("/static/js/htmx.min.js", "static/js/htmx.min.js")
+	// CSS
+	e.File("/static/css/main.css", "static/css/main.css")
+	// Font
+	e.File("/static/font/inter.css", "node_modules/@fontsource-variable/inter/index.css")
+	e.Static("/static/font/files/", "node_modules/@fontsource-variable/inter/files/")
+	// Icons
+	e.Static("/static/icons/", "node_modules/lucide-static/font/")
+}
 
 func main() {
 	e := echo.New()
@@ -22,10 +36,16 @@ func main() {
 	addStaticFiles(e)
 
 	e.GET("/", func(c echo.Context) error {
-		return render(c, http.StatusOK, views.Index())
+		return render(c, http.StatusOK, pages.Home())
 	})
 	e.GET("/setup", func(c echo.Context) error {
-		return render(c, http.StatusOK, views.Index())
+		return render(c, http.StatusOK, pages.Home())
+	})
+
+	groupModels := e.Group("/models")
+
+	groupModels.GET("/:id", func(c echo.Context) error {
+		return render(c, http.StatusOK, models.Show())
 	})
 
 	// Start
@@ -35,24 +55,23 @@ func main() {
 
 // Renders the given templ.Component.
 func render(ctx echo.Context, HTTPStatus int, t templ.Component) error {
+	// See https://htmx.org/docs/#caching
+	ctx.Response().Writer.Header().Add("Vary", "HX-Request")
+
 	ctx.Response().Writer.WriteHeader(HTTPStatus)
 
-	err := t.Render(context.Background(), ctx.Response().Writer)
+	var err error
+	// Only return the fragment (the given Component) on a HTMX request, else wrap it in the layout.
+	if ctx.Request().Header.Get("HX-Request") == "true" {
+		err = t.Render(context.Background(), ctx.Response().Writer)
+	} else {
+		wrapped := templ.WithChildren(context.Background(), t)
+		err = views.Layout().Render(wrapped, ctx.Response().Writer)
+	}
+
 	if err != nil {
 		return ctx.String(http.StatusInternalServerError, "failed to render response")
 	}
 
 	return nil
-}
-
-func addStaticFiles(e *echo.Echo) {
-	// HTMX
-	e.File("/static/js/htmx.min.js", "static/js/htmx.min.js")
-	// CSS
-	e.File("/static/css/main.css", "static/css/main.css")
-	// Font
-	e.File("/static/font/inter.css", "node_modules/@fontsource-variable/inter/index.css")
-	e.Static("/static/font/files/", "node_modules/@fontsource-variable/inter/files/")
-	// Icons
-	e.Static("/static/icons/", "node_modules/lucide-static/font/")
 }
